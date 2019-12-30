2963d68e585f173f1f5e86846a05eb76799d05d2
kangarooCoder <ksbilodeau07@gmail.com>
2019-12-30 15:31:11 -0600

    Commit #5: Constants and basic engine variables added and functionality for init method added

--- gameEngine/main.cpp
+++ gameEngine/main.cpp
@@ -5,14 +5,68 @@
 //  Created by Keegan Bilodeau on 12/30/19.
 //  Copyright © 2019 Keegan Bilodeau. All rights reserved.
 //
-
 #include <iostream>
+
 #include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+
+#include "main.hpp"
 
 bool init()
 {
     // Initialization success flag
     bool success = true;
+    
+    // Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        printf("SDL could not initialize! SDL Error: %s\n", SDL_GetError());
+        success = false;
+    }
+    // If SDL successfully initialized
+    else
+    {
+        // Creates a visible window
+        gWindow = SDL_CreateWindow("My Engine", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+        if (gWindow == nullptr)
+        {
+            printf("Window could not be created! SDL Error: %s\n", SDL_GetError());
+            success = false;
+        }
+        // If window could be created
+        else
+        {
+            // Create renderer with hardware acceleration and V-Sync enabled
+            gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+            if (gRenderer == nullptr)
+            {
+                printf("Renderer could not be created! SDL Error: %s\n", SDL_GetError());
+                success = false;
+            }
+            // If renderer could be created
+            else
+            {
+                // Initialize draw color
+                SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+                
+                // Initialize image loading (specifically pngs; however, more can be added)
+                int imgFlags = IMG_INIT_PNG;
+                if (!(IMG_Init(imgFlags) & imgFlags))
+                {
+                    printf("SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError());
+                    success = false;
+                }
+                
+                // Initalize font loading for future text rendering
+                if (TTF_Init() == -1)
+                {
+                    printf("SDL_ttf could not initialize! SDL_ttf Error: %s\n", TTF_GetError());
+                    success = false;
+                }
+            }
+        }
+    }
     
     return success;
 }
--- gameEngine/main.hpp
+++ gameEngine/main.hpp
@@ -6,8 +6,25 @@
 //  Copyright © 2019 Keegan Bilodeau. All rights reserved.
 //
 
-#ifndef main_h
-#define main_h
+#ifndef main_hpp
+#define main_hpp
+
+// Basic screen dimensions for window setup
+const int SCREEN_WIDTH = 600;
+const int SCREEN_HEIGHT = 600;
+
+// Game scale factor for tile textures
+const int SCALE_FACTOR = 3;
+
+// Tile dimensions for future tilemap system
+const int TILE_WIDTH = 16 * SCALE_FACTOR;
+const int TILE_HEIGHT = 16 * SCALE_FACTOR;
+
+// Main engine rendering window
+extern SDL_Window* gWindow;
+
+// Renderer for engine window
+extern SDL_Renderer* gRenderer;
 
 // Initializes libraries and game variables
 bool init();
@@ -18,4 +35,4 @@ bool loadMedia();
 // Closes SDL subsystems and frees various things such as textures
 void close();
 
-#endif /* main_h */
+#endif /* main_hpp */
